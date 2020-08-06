b52bdea130920eba48345288753b898e4536fdf0
Antonio O Fong <omarquinzan@yahoo.com>
2020-08-06 05:08:09 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,11 @@
-# require libraries/modules here
+ #require libraries/modules here
+ reuire'nokogiri'
 
 def create_project_hash
   # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  
+  kickstarter = Nokogiri::HTML(html)
+  
+  
 end
